15ad74633bd790087d5351ee3da383c4f83dd63e
igormilinchuk <144233134+igormilinchuk@users.noreply.github.com>
2024-05-17 13:37:20 +0300

    Update pageRoutes.js

--- front/pageRoutes.js
+++ front/pageRoutes.js
@@ -2,22 +2,18 @@ const express = require('express');
 const router = express.Router();
 const path = require('path');
 
-// Route for registration page
 router.get('/registration.html', (req, res) => {
   res.sendFile(path.join(__dirname, '../front/pages', 'registration.html'));
 });
 
-// Route for login page
 router.get('/login.html', (req, res) => {
   res.sendFile(path.join(__dirname, '../front/pages', 'login.html'));
 });
 
-// Route for authorized site page
 router.get('/authorizedSite.html', (req, res) => {
   res.sendFile(path.join(__dirname, '../front/pages', 'authorizedSite.html'));
 });
 
-// Route for active sessions page
 router.get('/activeSessions.html', (req, res) => {
   res.sendFile(path.join(__dirname, '../front/pages', 'activeSessions.html'));
 });
