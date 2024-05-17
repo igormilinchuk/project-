9e102479421960b442affb7b356bad905b2b3958
igormilinchuk <144233134+igormilinchuk@users.noreply.github.com>
2024-05-17 14:22:49 +0300

    use env in config

---
+++ backend/config/config.js
@@ -0,0 +1,17 @@
+require('dotenv').config();
+
+let config =  {
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    dialect: process.env.DB_DIALECT,
+  };
+
+
+  module.exports = {
+    development: config,
+    test: config,
+    production: config
+  }
